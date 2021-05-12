f8f424f65212098e888536fccb873a35222191fa
Nico Rosner <gaucho@amazon.com>
2021-05-12 11:07:19 -0400

    Add tiny project with 1 class, using Maven as build system

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gurutest
+
+This is a test of Guru Reviewer integration with Github Actions.
+
+### How to build the project locally
+
+`mvn package`
+
+### How to run the `App` main class
+
+`java -cp target/gurutest-1.0-SNAPSHOT.jar com.amazon.test.App`
---
+++ src/main/java/com/amazon/test/App.java
@@ -0,0 +1,23 @@
+package com.amazon.test;
+
+import javax.crypto.Cipher;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.NoSuchPaddingException;
+
+public class App 
+{
+	public static void good() throws NoSuchAlgorithmException, NoSuchPaddingException {
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+	}
+
+	public static void bad() throws NoSuchAlgorithmException, NoSuchPaddingException {
+		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+	}
+
+	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
+		System.out.println("Welcome to this little App!");
+		good();
+		bad();
+		System.exit(0);
+	}
+}
